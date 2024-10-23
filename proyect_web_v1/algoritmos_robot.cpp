24f048982ab423d5d4ac048cb76c6b80603f5f05
ibarramiguel119 <ibarramiguel119@gmail.com>
2024-10-23 12:21:57 -0600

    Fixed library of C++ for the robot

--- proyect_web_v1/algoritmos_robot.cpp
+++ proyect_web_v1/algoritmos_robot.cpp
@@ -1,5 +1,4 @@
 #include <pybind11/pybind11.h>
-#include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <pybind11/stl.h>
 #include <pybind11/functional.h>
@@ -41,11 +40,24 @@ double radianes_a_grados(double radianes);
 double gradosARadianes(double grados);
 std::tuple<double, double, double> sph2cart(double r, double theta, double phi);
 std::vector<double> generarPuntos(int start, int end, int new_roll);
-std::string floatToString(double value, int precision);
+
+extern "C" {
+    std::string floatToString(float value, int precision);
+}
+
+
 bool enviarDatosCaracterPorCaracter(int serial_fd, const std::string& datos);
 std::string recibirDatosBloqueante(int serial_fd);
 void cerrarPuertoSerie(int serial_fd);
 std::vector<double> grados_a_radianes(const std::vector<int>& grados);
+
+
+
+
+void printVectorOfVectors(const std::vector<std::vector<int>>& vec);
+void delaySeconds(int seconds);
+
+
 
 
 
@@ -110,92 +122,6 @@ std::vector<std::vector<int>> CalcularPuntosMovimiento(const std::vector<int>& a
 }
 
 
-// La función principal de Cinemática Inversa
-std::vector<std::vector<int>> CalcularPuntosCinematicaInversa(const std::vector<std::vector<int>>& result, int si, int slider0Value, int new_roll, py::function callback) {
-    std::vector<std::vector<int>> D; 
-    int n = 0;
-    int l2 = 300;  // Longitud del eslabón
-    int cont = 1;
-
-    // Configuración del puerto serie en Linux
-    std::string puertoEspecifico = "/dev/ttyUSB0";  // Cambiar al puerto serie específico en Linux
-    int serial_fd = abrirPuertoSerie(puertoEspecifico);
-
-    if (serial_fd == -1) {
-        std::cerr << "Error al abrir el puerto" << std::endl;
-        return D;  // Salir de la función si no se puede abrir el puerto
-    }
-
-    auto numerototal = si * new_roll;
-
-    while (n <= si) {
-        std::vector<int> temp = result[n];
-        auto x = slider0Value;
-
-        std::cout << "Slider value: " << slider0Value << std::endl;
-        std::cout << "Temp vector: ";
-        for (int elem : temp) {
-            std::cout << elem << " ";
-        }
-        std::cout << std::endl;
-
-        // Convertir grados a radianes
-        std::vector<double> rads = grados_a_radianes(temp);
-        std::cout << "Rads: " << rads[0] << " " << rads[1] << std::endl;
-
-        // Convertir coordenadas esféricas a cartesianas
-        auto resultado = sph2cart(rads[0], rads[1], temp[2]);
-        double px = std::get<0>(resultado);
-        double py = std::get<1>(resultado);
-        double pz = std::get<2>(resultado);
-        std::cout << "Cartesian coordinates (px, py, pz): " << px << ", " << py << ", " << pz << std::endl;
-
-        // Puntos generados para q5
-        std::vector<double> puntos = generarPuntos(0, 180, new_roll);
-
-        for (double q5 : puntos) {
-            auto q1 = atan(-px / py) + 3.1416;
-            auto q2 = px * sin(q1) - py * cos(q1); 
-            auto q3 = pz;
-            auto q4 = atan(-(q2 + 515) / q3);
-
-            std::string q1_str = floatToString(q1, 4);
-            std::string q2_str = floatToString(q2, 4);
-            std::string q3_str = floatToString(q3, 4);
-            std::string q4_str = floatToString(q4, 4);
-            std::string q5_str = floatToString(q5, 4);
-            std::string datosAEnviar = q1_str + "a" + q2_str + "b" + q3_str + "c" + q4_str + "d" + q5_str + ">";
-
-            auto grados1 = radianes_a_grados(q1);
-            std::cout << "Grados de q1: " << grados1 << std::endl;
-
-            if (enviarDatosCaracterPorCaracter(serial_fd, datosAEnviar)) {
-                std::cout << "Datos enviados: " << datosAEnviar << std::endl;
-                std::string datosRecibidos = recibirDatosBloqueante(serial_fd);
-                if (!datosRecibidos.empty()) {
-                    std::cout << "Datos recibidos: " << datosRecibidos << std::endl;
-                    if (datosRecibidos == "1") {
-                        py::gil_scoped_acquire acquire;
-                        callback(grados1, numerototal);
-                        py::gil_scoped_release release;
-                    }
-                } else {
-                    std::cout << "No se recibieron datos." << std::endl;
-                }
-            } else {
-                std::cerr << "Error al enviar datos." << std::endl;
-            }
-        }
-        D.push_back(temp);
-        n++;
-        cont++;
-        if (cont == x + 2) {
-            cont = 1;
-        }
-    }
-    cerrarPuertoSerie(serial_fd);
-    return D;
-}
 
 
 // Función para abrir el puerto serie en Linux
@@ -308,6 +234,173 @@ double gradosARadianes(double grados) {
 
 
 
+/// Imprimir los datos del vector Result para comparar en matlab  
+void printVectorOfVectors(const std::vector<std::vector<int>>& vec) {
+    for (const auto& innerVec : vec) {
+        std::cout << '[';
+        for (size_t i = 0; i < innerVec.size(); ++i) {
+            std::cout << innerVec[i];
+            if (i < innerVec.size() - 1) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << ']' << std::endl;
+    }
+}
+
+void delaySeconds(int seconds) {
+    std::this_thread::sleep_for(std::chrono::seconds(seconds));
+}
+
+
+// La función principal de Cinemática Inversa
+std::vector<std::vector<int>> CalcularPuntosCinematicaInversa(const std::vector<std::vector<int>>& result, int si, int slider0Value, int new_roll, py::function callback) {
+    std::vector<std::vector<int>> D; 
+    int n = 0;
+    //int l2 = 300;  // Longitud del eslabón
+    int cont = 1;
+
+    // Configuración del puerto serie en Linux
+    std::string puertoEspecifico = "/dev/ttyUSB0";  // Cambiar al puerto serie específico en Linux
+    int serial_fd = abrirPuertoSerie(puertoEspecifico);
+
+    if (serial_fd == -1) {
+        std::cerr << "Error al abrir el puerto" << std::endl;
+        return D;  // Salir de la función si no se puede abrir el puerto
+    }
+
+    auto numerototal = si * new_roll;
+
+    while (n <= si) {
+        std::vector<int> temp = result[n];
+        auto x = slider0Value;
+
+        std::cout << "Slider value: " << slider0Value << std::endl;
+        std::cout << "Temp vector: ";
+        for (int elem : temp) {
+            std::cout << elem << " ";
+        }
+        std::cout << std::endl;
+
+        // Convertir grados a radianes
+        std::vector<double> rads = grados_a_radianes(temp);
+        std::cout << "Rads: " << rads[0] << " " << rads[1] << std::endl;
+
+        // Convertir coordenadas esféricas a cartesianas
+        auto resultado = sph2cart(rads[0], rads[1], temp[2]);
+        double px = std::get<0>(resultado);
+        double py = std::get<1>(resultado);
+        double pz = std::get<2>(resultado);
+        std::cout << "Cartesian coordinates (px, py, pz): " << px << ", " << py << ", " << pz << std::endl;
+
+        // Puntos generados para q5
+        std::vector<double> puntos = generarPuntos(0, 180, new_roll);
+
+        for (double q5 : puntos) {
+            auto q1 = atan(-px / py) + 3.1416;
+            auto q2 = px * sin(q1) - py * cos(q1); 
+            auto q3 = pz;
+            auto q4 = atan(-(q2 + 515) / q3);
+
+            std::string q1_str = floatToString(q1, 4);
+            std::string q2_str = floatToString(q2, 4);
+            std::string q3_str = floatToString(q3, 4);
+            std::string q4_str = floatToString(q4, 4);
+            std::string q5_str = floatToString(q5, 4);
+            std::string datosAEnviar = q1_str + "a" + q2_str + "b" + q3_str + "c" + q4_str + "d" + q5_str + ">";
+
+            auto grados1 = radianes_a_grados(q1);
+            std::cout << "Grados de q1: " << grados1 << std::endl;
+
+            if (enviarDatosCaracterPorCaracter(serial_fd, datosAEnviar)) {
+                std::cout << "Datos enviados: " << datosAEnviar << std::endl;
+                std::string datosRecibidos = recibirDatosBloqueante(serial_fd);
+                if (!datosRecibidos.empty()) {
+                    std::cout << "Datos recibidos: " << datosRecibidos << std::endl;
+                    if (datosRecibidos == "1") {
+                        py::gil_scoped_acquire acquire;
+                        callback(grados1, numerototal);
+                        py::gil_scoped_release release;
+                    }
+                } else {
+                    std::cout << "No se recibieron datos." << std::endl;
+                }
+            } else {
+                std::cerr << "Error al enviar datos." << std::endl;
+            }
+        }
+        D.push_back(temp);
+        n++;
+        cont++;
+        if (cont == x + 2) {
+            cont = 1;
+        }
+    }
+    cerrarPuertoSerie(serial_fd);
+    return D;
+}
+
+
+
+
+
+void procesarDatos(int slider1Value, int slider0Value, int slider2Value,int new_roll,py::function callback) {
+    int GAltitude, GAsimut, GRoll;
+    //int radioEsfera = 299;
+
+    CalcularGrados(slider1Value, slider0Value, slider2Value, GAltitude, GAsimut, GRoll);
+    std::cout << "Grados en Altitude: " << GAltitude << std::endl;
+    std::cout << "Grados en Asimuth: " << GAsimut << std::endl;
+    std::cout << "Grados en Roll:" << GRoll << std::endl;
+
+    //Muestra el arreglo de distribuccion de angulos de Azimuth
+    std::vector<int> resultado1 = CalcularArreglo1(GAsimut);
+    // Imprimir el contenido del vector devuelto
+    std::cout << "Los datos del arreglo 1:" << std::endl;
+    for (int value1 : resultado1) {
+        std::cout << value1 << " ";
+    }
+    std::cout << std::endl;
+
+    //Muestra el arreglo de distribuccion de angulos de Altitude
+    std::vector<int> resultado2 = CalcularArreglo2(GAltitude);
+    // Imprimir el contenido del vector devuelto
+    std::cout << "Los datos del arreglo 2:" << std::endl;
+    for (int value2 : resultado2) {
+        std::cout << value2 << " ";
+    }
+    std::cout << std::endl;
+
+    //Muestra el arreglo de distribuccion de grados de Roll
+    std::vector<int> resultado3 = CalcularArreglo3(GRoll);
+    // Imprimir el contenido del vector devuelto
+    std::cout << "Los datos del arreglo 3:" << std::endl;
+    for (int value3 : resultado3) {
+        std::cout << value3 << " ";
+    }
+    std::cout << std::endl;
+
+    // Esta funcion calcula los puntos de posicionamiento de la circunferencia
+    std::vector<std::vector<int>> result = CalcularPuntosMovimiento(resultado1, resultado2, slider2Value);
+    printVectorOfVectors(result);
+
+    // Imprimir el resultado
+    // Calculo del tamaño del tamaño de la forma en se movera el robot 
+    int si = (resultado1.size() - 1) * (resultado2.size() - 1);
+    std::cout << si << std::endl;
+
+    ///Imprimir los datos de vector D por separado
+    //CalcularPuntosCinematicaInversa(result, si,callback); 
+    CalcularPuntosCinematicaInversa(result, si,slider0Value,new_roll,callback); 
+}
+
+
+
+
+
+
+
+
 // Módulo que expone las funciones del Robot 
 PYBIND11_MODULE(algoritmos_robot, m) {
     m.def("suma", &suma, "Una función que suma dos números");
@@ -324,4 +417,6 @@ PYBIND11_MODULE(algoritmos_robot, m) {
     m.def("CalcularArreglo2", &CalcularArreglo2, "Función para calcular arreglo 2");
 
     m.def("CalcularArreglo3", &CalcularArreglo3, "Función para calcular arreglo 3");
+
+    m.def("procesarDatos", &procesarDatos,"Esta funcion simplifica la llamadas en el programa principal");
 }
