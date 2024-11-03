259c3f9fc8cfe302c5f3ff3ab7f528fed3c4bbb7
ibarramiguel119 <ibarramiguel119@gmail.com>
2024-11-03 13:21:25 -0600

    Fixed frontend and added new features

--- proyect_web_v1/base_app/urls.py
+++ proyect_web_v1/base_app/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.home_system, name='mi_vista'),
 
@@ -28,5 +31,5 @@ urlpatterns = [
     
     path('calcular-grados/', views.calcular_grados_view, name='calcular_grados'),
     
-]
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
--- proyect_web_v1/proyect_web_v1/settings.py
+++ proyect_web_v1/proyect_web_v1/settings.py
@@ -170,12 +170,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 import os
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Directorio de archivos estáticos personalizados
+    os.path.join(BASE_DIR, "app/static"),
 ]
+
+#Cargar las imagenes de los autos 
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
