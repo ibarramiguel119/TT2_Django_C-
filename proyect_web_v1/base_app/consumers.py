b513acec49cb3de7742b2148b1af45254ac4cd0b
ibarramiguel119 <ibarramiguel119@gmail.com>
2024-11-13 23:58:18 -0600

    Added the second task to handle the capture of images

--- proyect_web_v1/base_app/consumers.py
+++ proyect_web_v1/base_app/consumers.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
-from.tasks import procesar_datos
+from.tasks import procesar_datos,save_image
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 import logging
@@ -42,6 +42,7 @@ class CaptureConsumer(AsyncWebsocketConsumer):
 
         # Handle start_capture action
         if action == 'start_capture':
+            
 
             radio_option = data.get('radio_option', None)  # Obtiene el valor del radio button
 
@@ -67,6 +68,8 @@ class CaptureConsumer(AsyncWebsocketConsumer):
 
                 # Send capture request to Celery
                 procesar_datos.delay(slider1, slider2, slider4, slider3)
+                
+
             elif radio_option == '2':
                 select_option = data.get('select_option') 
                 
@@ -121,12 +124,25 @@ class CaptureConsumer(AsyncWebsocketConsumer):
                 'q1': q1,
                 'numerototal': numerototal
             }))
+         
+        if action == 'send.imagen_clint':
+            # Maneja el mensaje enviado desde Celery
+            image_url = data.get('image_url')
+            session_folder = data.get('session_folder')
+
+            # Aquí puedes enviar un mensaje de vuelta al cliente WebSocket
+            await self.send(text_data=json.dumps({
+                'action': 'capturar_imagen',
+                'image_url': image_url,
+                'session_folder': session_folder,
+            }))
+
+
         
         # Handle save_image action
         elif action == 'save_image':
             image_data = data['image']
-            # Save the image asynchronously
-            await self.save_image(image_data)
+            save_image.delay(image_data,self.session_folder,)
             
  
 
@@ -151,26 +167,7 @@ class CaptureConsumer(AsyncWebsocketConsumer):
             'numerototal': numerototal
         }))
 
-    async def save_image(self, image_data):
-        # Extract the format and content of the image
-        format, imgstr = image_data.split(';base64,')
-        ext = format.split('/')[-1]
-        image_content = ContentFile(base64.b64decode(imgstr), name=f'capture.{ext}')
-
-        # Save the image in the session-specific folder
-        file_path = await sync_to_async(self._save_image)(image_content, ext)
-        print(f'Image saved at: {file_path}')
-        
 
 
-    def _save_image(self, image_content, ext):
-        # This method saves the image inside the session-specific folder
-        session_path = os.path.join('captured_images', self.session_folder)
-        
-        # Ensure the session folder exists
-        if not os.path.exists(session_path):
-            os.makedirs(session_path)
 
-        # Save the image within the session folder
-        file_path = default_storage.save(os.path.join(session_path, f'capture.{ext}'), image_content)
-        return file_path
+    
--- proyect_web_v1/base_app/tasks.py
+++ proyect_web_v1/base_app/tasks.py
@@ -4,6 +4,14 @@ from asgiref.sync import async_to_sync
 import json
 import logging
 import algoritmos_robot
+import time
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+import base64
+import os
+import json
+
+from channels.layers import get_channel_layer
 
 logger = logging.getLogger(__name__)
 
@@ -37,3 +45,55 @@ def procesar_datos(slider1, slider2, slider3, slider4):
                                             lambda q1, numerototal: captura_imagen(q1, numerototal))
 
     return result
+
+@shared_task
+def save_image(image_data, session_folder):
+  
+    try:
+        # Extraer el formato y contenido de la imagen
+        format, imgstr = image_data.split(';base64,')  # Imagen en base64
+        ext = format.split('/')[-1]  # Obtenemos la extensión de la imagen
+        image_content = ContentFile(base64.b64decode(imgstr), name=f'capture.{ext}')
+
+        # Guardamos la imagen en el servidor en una carpeta específica de la sesión
+        file_path = _save_image(image_content, ext, session_folder)
+        print(f'Image saved at: {file_path}')
+
+        # Obtener la URL de la imagen guardada
+        image_url = default_storage.url(file_path)  # Generar la URL pública
+
+        # Enviar la URL de la imagen al cliente a través de WebSocket usando Channels
+        # Enviar el mensaje al grupo WebSocket con un 'type' adecuado
+        # Después de guardar la imagen, enviar un mensaje con el 'type' al grupo WebSocket
+        channel_layer = get_channel_layer()
+        logger.info("El canal el siguiente: %s", channel_layer)
+        try:
+            logger.info("Preparando para enviar el mensaje a capture_group")
+            message = {
+                "type": "send.imagen_clint",  # Asegúrate de que este tipo sea manejado en tu consumidor
+                "message": "capturar_imagen",
+                'image_url': image_url,  # Datos asociados al mensaje
+                'session_folder': session_folder,
+                
+            }
+            logger.info("Enviando mensaje: %s", message)
+            async_to_sync(channel_layer.group_send)("capture_group", message)
+
+        except Exception as e:
+            logger.error("Error al enviar la direccion de la carpeta donde se guarda la informacion: %s", e)
+    except Exception as e:
+        print(f"Error al guardar la imagen: {e}")
+
+def _save_image(image_content, ext, session_folder):
+    """
+    Método sincronizado para guardar la imagen en una carpeta de sesión
+    """
+    session_path = os.path.join('captured_images', session_folder)
+
+    # Asegurarse de que la carpeta de sesión exista
+    if not os.path.exists(session_path):
+        os.makedirs(session_path)
+
+    # Guardar la imagen dentro de la carpeta de sesión
+    file_path = default_storage.save(os.path.join(session_path, f'capture.{ext}'), image_content)
+    return file_path
